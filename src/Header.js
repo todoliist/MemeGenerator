1d81f8ba33534d5393aa968dbc7074080706319b
Tim Su <tsu@fieldflex.com>
2020-08-03 19:58:59 -0600

    first commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="http://www.pngall.com/wp-content/uploads/5/Troll-Transparent.png" 
+                alt="Problem?"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
